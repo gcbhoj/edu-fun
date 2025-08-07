1d5fe1fee5747a1330ba042fe35096d5925d98a0
BhojGC <gcbhoj@gmail.com>
2025-08-06 22:26:26 -0400

    connected to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,24 @@
 require("dotenv").config();
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 const uri = process.env.ATLAS_URI;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("MongoDB Connection established");
+  })
+  .catch((error) => {
+    console.error("MongoDb connection Failed:", error.message);
+  });
 
 app.listen(port, () => {
-    console.log(`Server running on port:${port}`)
-})
+  console.log(`Server running on port:${port}`);
+});
