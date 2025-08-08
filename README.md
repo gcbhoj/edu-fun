1a021adebc8a141f664aa6b07878e1e0e2fc5662
BhojGC <gcbhoj@gmail.com>
2025-08-08 00:33:21 -0400

    Added comments to the register user method added some format to the readme file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This project aims to develop a supplemental educational curriculum designed to support students in building essential skills beyond traditional academic content. The platform will offer a rich, engaging experience by integrating interactive avatars and gamified learning environments tailored to different age groups and school years.
+# This project aims to develop a supplemental educational curriculum designed to support students in building essential skills beyond traditional academic content. The platform will offer a rich, engaging experience by integrating interactive avatars and gamified learning environments tailored to different age groups and school years
 
 Key Features
 
@@ -19,5 +19,28 @@ Completion of daily challenges unlocks level-ups, digital rewards, and avatar ac
 Early Childhood Focus:
 Rooted in the principle that early childhood education thrives on association and sensory interaction, younger learners will explore foundational concepts such as colors, alphabets, and numbers through playful and intuitive activities.
 
-***NOTE*****
-- Version 1 will focus completely on Mathematics. Like learning numbers, simple addition
+***NOTE***
+  Version 1 will focus completely on Mathematics. Like learning numbers, simple addition
+Game Types to Support Cognitive Growth
+Memory Match (match cards)
+
+Sorting Games (colors, shapes)
+
+Counting Objects
+
+Find the Pattern
+
+Quick Math (1–10)
+
+“Which one doesn’t belong?” puzzles
+
+Age-Based Difficulty Levels
+Break content into age/level-based groups:
+
+Age 4–5: Counting, matching shapes
+
+Age 5–6: Simple addition, recognizing patterns
+
+Age 6–7: Subtraction, ordering numbers
+
+Age 7–8: Basic word problems, logic puzzles
--- backend/Controller/UserController.js
+++ backend/Controller/UserController.js
@@ -6,14 +6,15 @@ const { generateUniqueId } = require("../Utils/UniqueIdGenerator");
 
 const registerUser = async (req, res) => {
   try {
+    //parsing userdata from request
     const { name, email, password } = req.body;
-
+    // checking for null values
     if (!name || !email || !password) {
       return res
         .status(400)
         .json({ status: "failure", message: "All Fields are Required" });
     }
-
+    // normalizing name to lowercase and checking for username availability
     const formattedName = name.trim().toLowerCase();
 
     const verifyUserName = await userModel.findOne({ name: formattedName });
@@ -23,19 +24,24 @@ const registerUser = async (req, res) => {
         .json({ status: "failure", message: "User Name Already Taken." });
     }
 
+    /*using validator to check the email format is correct or not
+      NOTE: Validator only checks for email format not validity of the email
+    */
+
     if (!validator.isEmail(email)) {
       return res
         .status(400)
         .json({ status: "failure", message: "Email Format Not Valid." });
     }
 
-    let verifyUserEmail = await userModel.findOne({email: email });
+    let verifyUserEmail = await userModel.findOne({ email: email });
     if (verifyUserEmail) {
       return res.status(409).json({
         status: "failure",
         message: "User with the given email already exists.",
       });
     }
+    /* using validtor of check the strength of the password and its compliance*/
 
     if (!validator.isStrongPassword(password)) {
       return res.status(400).json({
@@ -45,16 +51,18 @@ const registerUser = async (req, res) => {
       });
     }
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    const newUserId = generateUniqueId("u");
+    const salt = await bcrypt.genSalt(10); // generating salt to add to hashed password
+    const hashedPassword = await bcrypt.hash(password, salt); // using bcrypt to hash the password and salting
+    const newUserId = generateUniqueId("u"); // generating a unique id that is divided into  date, time and a rondom 4 digit number.
 
+    //Inserting the data into the user Model.
     const newUser = new userModel({
       _id: newUserId,
       name: formattedName,
       email,
       password: hashedPassword,
     });
+    // saving the user data to database.
     const response = await newUser.save();
 
     if (!response) {
